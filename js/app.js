216141220b3e7893760479695845156ec40ca185
John Bouakhasith <johnbeedot@gmail.com>
2023-03-07 11:10:19 -0500

    modified styles and index

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = [
-  "Junior Web Developer",
-  "Self-Taught Developer",
-  "IT Professional",
-  "Cat Dad",
-];
+const textArray = ["Aspiring Developer", "IT Professional", "Cat Dad"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
