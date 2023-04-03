12db66ea74a3f7e26c6acd83111b44249bc9a4a7
John Bouakhasith <johnbeedot@gmail.com>
2023-04-02 20:57:19 -0400

    hamburger menu now collapse after clicking link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -86,24 +86,28 @@ export default function Navbar() {
           <a
             href="#"
             className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-500 hover:text-gray-200"
+            onClick={() => setIsOpen(false)}
           >
             Home
           </a>
           <a
             href="#"
             className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-500 hover:text-gray-200"
+            onClick={() => setIsOpen(false)}
           >
             Blog
           </a>
           <a
             href="#"
             className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-500 hover:text-gray-200"
+            onClick={() => setIsOpen(false)}
           >
             Portfolio
           </a>
           <a
             href="#"
             className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-500 hover:text-gray-200"
+            onClick={() => setIsOpen(false)}
           >
             About
           </a>
