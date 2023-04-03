7d7795d4e4794663e8962f579513f2e337125b1b
John Bouakhasith <johnbeedot@gmail.com>
2023-04-02 20:53:45 -0400

    change hamburger menu colors

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -85,25 +85,25 @@ export default function Navbar() {
         >
           <a
             href="#"
-            className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-200"
+            className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-500 hover:text-gray-200"
           >
             Home
           </a>
           <a
             href="#"
-            className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-200"
+            className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-500 hover:text-gray-200"
           >
             Blog
           </a>
           <a
             href="#"
-            className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-200"
+            className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-500 hover:text-gray-200"
           >
             Portfolio
           </a>
           <a
             href="#"
-            className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-200"
+            className="block px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-500 hover:text-gray-200"
           >
             About
           </a>
