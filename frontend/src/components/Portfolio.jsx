be81706f1a3c5d3607a63df26b424a785f35601c
John Bouakhasith <johnbeedot@gmail.com>
2023-04-04 11:08:00 -0400

    added another project column

--- frontend/src/components/Portfolio.jsx
+++ frontend/src/components/Portfolio.jsx
@@ -18,38 +18,67 @@ export default function Portfolio() {
             </p>
           </div>
 
-          {/* Project */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
-            <div>
-              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                <img
-                  src="./src/img/coming-soon.jpg"
-                  alt="Project Image"
-                  className="h-64 w-full object-cover object-center"
-                />
-                <div className="p-6">
-                  <h2 className="text-xl text-gray-900 font-semibold mb-2">
-                    Therapy With Esther
-                  </h2>
-                  <p className="text-gray-500 text-sm mb-4">
-                    Technologies used: React, Tailwind CSS
-                  </p>
-                  <p className="text-md text-gray-900 mb-4">
-                    This website was created for a relationship therapist who
-                    specializes in helping individuals and couples build healthy
-                    and fulfilling relationships. The website features a modern
-                    and inviting design, with easy-to-use navigation and
-                    informative content about the therapist's services and
-                    approach.
-                  </p>
-                  <p className="text-md text-gray-900 mb-4">
-                    Still under construction.
-                  </p>
+            {/* Project */}
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+              <img
+                src="./src/img/coming-soon.jpg"
+                alt="Project Image"
+                className="h-64 w-full object-cover object-center"
+              />
+              <div className="p-6">
+                <h2 className="text-xl text-gray-900 font-semibold mb-2">
+                  Therapy With Esther
+                </h2>
+                <p className="text-gray-500 text-sm mb-4">
+                  Technologies used: React, Tailwind CSS
+                </p>
+                <p className="text-md text-gray-900 mb-4">
+                  This website was created for a relationship therapist who
+                  specializes in helping individuals and couples build healthy
+                  and fulfilling relationships. The website features a modern
+                  and inviting design, with easy-to-use navigation and
+                  informative content about the therapist's services and
+                  approach.
+                </p>
+                <p className="text-md text-gray-900 mb-4">
+                  Still under construction.
+                </p>
 
-                  <a href="#" className="text-blue-500 hover:text-blue-700">
-                    View Project &rarr;
-                  </a>
-                </div>
+                <a href="#" className="text-blue-500 hover:text-blue-700">
+                  View Project &rarr;
+                </a>
+              </div>
+            </div>
+
+            {/* Project */}
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+              <img
+                src="./src/img/coming-soon.jpg"
+                alt="Project Image"
+                className="h-64 w-full object-cover object-center"
+              />
+              <div className="p-6">
+                <h2 className="text-xl text-gray-900 font-semibold mb-2">
+                  NotJustSnkrs
+                </h2>
+                <p className="text-gray-500 text-sm mb-4">
+                  Technologies used: HTML, CSS, JavaScript, WooCommerce
+                </p>
+                <p className="text-md text-gray-900 mb-4">
+                  This was created using WooCommerce to provide a seamless
+                  shopping experience for sneakerheads and streetwear
+                  enthusiasts. The website offers a wide selection of shoes,
+                  clothing, and accessories from top brands such as Nike,
+                  Adidas, and Supreme.
+                </p>
+                <p className="text-md text-gray-900 mb-4">
+                  Still under construction.
+                </p>
+
+                <a href="#" className="text-blue-500 hover:text-blue-700">
+                  View Project &rarr;
+                </a>
               </div>
             </div>
           </div>
